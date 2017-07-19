975a1912c811ec8159c3b2ad3e6574c8c8240705
Michał Kuśmierczyk <mk@greensteam.com>
2017-07-20 00:46:30 +0200

    improve gitignore and setup script; begin work on Not.hdl

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
+mv nand2tetris nand2tetris_temp
 wget -O nand2tetris.zip http://nand2tetris.org/software/nand2tetris.zip
 unzip nand2tetris.zip
 rm nand2tetris.zip
 chmod +x nand2tetris/tools/*.sh
+cp -r nand2tetris_temp/* nand2tetris
+rm -rf nand2tetris_temp
