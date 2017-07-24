27c5e6caafb1d6166812526f1180a7273f501b09
Michal Kusmierczyk <kusm@protonmail.ch>
2017-07-24 02:53:39 +0200

    improve testing script; prepare for project 4

--- test.sh
+++ test.sh
@@ -1,29 +1,59 @@
-echo "-- Project 1 --"
-echo ""
 
-for TEST in nand2tetris/projects/01/*.tst
-do
-	echo $TEST
-	./nand2tetris/tools/HardwareSimulator.sh $TEST
+PROJECT=$1
+if [[ $# -eq 0 ]]; then
+	PROJECT="all"
+fi
+
+if [[ $PROJECT =~ ^(1|all)$ ]]; then
+	echo "-- Project 1 --"
 	echo ""
-done
 
-echo "-- Project 2 --"
-echo ""
+	for TEST in nand2tetris/projects/01/*.tst
+	do
+		echo $TEST
+		./nand2tetris/tools/HardwareSimulator.sh $TEST
+		echo ""
+	done
+fi
 
-for TEST in nand2tetris/projects/02/*.tst
-do
-	echo $TEST
-	./nand2tetris/tools/HardwareSimulator.sh $TEST
+if [[ $PROJECT =~ ^(2|all)$ ]]; then
+	echo "-- Project 2 --"
 	echo ""
-done
 
-echo "-- Project 3 --"
-echo ""
+	for TEST in nand2tetris/projects/02/*.tst
+	do
+		echo $TEST
+		./nand2tetris/tools/HardwareSimulator.sh $TEST
+		echo ""
+	done
+fi
 
-for TEST in nand2tetris/projects/03/*/*.tst
-do
-	echo $TEST
-	./nand2tetris/tools/HardwareSimulator.sh $TEST
+if [[ $PROJECT =~ ^(3|all)$ ]]; then
+	echo "-- Project 3 --"
 	echo ""
-done
+
+	for TEST in nand2tetris/projects/03/*/*.tst
+	do
+		echo $TEST
+		./nand2tetris/tools/HardwareSimulator.sh $TEST
+		echo ""
+	done
+fi
+
+if [[ $PROJECT =~ ^(4|all)$ ]]; then
+	echo "-- Project 4 --"
+	echo ""
+
+	for ASMFILE in nand2tetris/projects/04/*/*.asm
+	do
+		./nand2tetris/tools/Assembler.sh $ASMFILE
+	done
+
+	echo ""
+	for TEST in nand2tetris/projects/04/*/*.tst
+	do
+		echo $TEST
+		./nand2tetris/tools/CPUEmulator.sh $TEST
+		echo ""
+	done
+fi
