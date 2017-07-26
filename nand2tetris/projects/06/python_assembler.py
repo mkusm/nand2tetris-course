a39912a433275f9319317b08f52a8036a88ab88b
Michał Kuśmierczyk <mk@greensteam.com>
2017-07-26 19:29:36 +0200

    python assembler parsing correctly files w/o labels

--- nand2tetris/projects/06/python_assembler.py
+++ nand2tetris/projects/06/python_assembler.py
@@ -2,7 +2,6 @@ import sys
 import os
 
 
-
 def parse_A_instruction(line):
     number = int(line[1:])
 
@@ -14,8 +13,89 @@ def parse_A_instruction(line):
     return binary
 
 
+def split_C_instruction(line):
+    assert line.count('=') in [0, 1]
+    assert line.count(';') in [0, 1]
+    if '=' in line:
+        dest, other = line.split('=')
+    else:
+        dest = None
+        other = line
+    if ';' in other:
+        comp, jump = other.split(';')
+    else:
+        comp = other
+        jump = None
+    return comp, dest, jump
+
+
+def calculate_comp(comp):
+    if not comp:
+        raise ValueError('Missing computation')
+    a_0_comp_dict = {
+        '0':   '101010',
+        '1':   '111111',
+        '-1':  '111010',
+        'D':   '001100',
+        'A':   '110000',
+        '!D':  '001101',
+        '!A':  '110001',
+        '-D':  '001111',
+        '-A':  '110011',
+        'D+1': '011111',
+        'A+1': '110111',
+        'D-1': '001110',
+        'A-1': '110010',
+        'D+A': '000010',
+        'D-A': '010011',
+        'A-D': '000111',
+        'D&A': '000000',
+        'D|A': '010101'
+    }
+    binary = a_0_comp_dict.get(comp, False)
+    if binary:
+        return '0' + binary
+    else:
+        if not 'M' in comp:
+            raise ValueError('Wrong computation')
+        comp = comp.replace('M', 'A')
+        binary = a_0_comp_dict.get(comp, False)
+        if not binary:
+            raise ValueError('Wrong computation')
+        return '1' + binary
+
+
+def calculate_dest(dest):
+    result = ['0', '0', '0']
+    if dest is None:
+        return '000'
+    if 'M' in dest:
+        result[2] = '1'
+    if 'D' in dest:
+        result[1] = '1'
+    if 'A' in dest:
+        result[0] = '1'
+    result = "".join(result)
+    return result
+
+def calculate_jump(jump):
+    jumps = (None, 'JGT', 'JEQ', 'JGE', 'JLT', 'JNE', 'JLE', 'JMP')
+    for index, jump_expected in enumerate(jumps):
+        if jump_expected == jump:
+            return format(index, '03b')
+    else:
+        raise ValueError(f"Jump {jump} is not valid")
+
 def parse_C_instruction(line):
-    pass
+    assert len(line) > 0
+    comp, dest, jump = split_C_instruction(line)
+    comp = calculate_comp(comp)
+    dest = calculate_dest(dest)
+    jump = calculate_jump(jump)
+    assert len(comp) == 7
+    assert len(dest) == 3
+    assert len(jump) == 3
+    return '111' + comp + dest + jump
 
 
 def parse_line(line):
@@ -24,39 +104,50 @@ def parse_line(line):
     else:
         return parse_C_instruction(line)
 
-
-def assemble():
+def check_arguments():
     if not len(sys.argv) == 1:
         raise ValueError('Expected 1 argument, received: ', sys.argv)
 
     asm_file_path = sys.argv[0]
 
     if not os.path.exists(asm_file_path):
-        raise ValueError('Wrong argument: ', sys.argv, ', file does not exist')
+        raise ValueError('Wrong argument: ', asm_file_path, ', file does not exist')
 
     asm_file_path = os.path.abspath(asm_file_path)
-    asm_file_no_extension, asm_file_extension = (
-        os.path.splitext(asm_file_path)
-    )
 
+    _, asm_file_extension = os.path.splitext(asm_file_path)
     if not asm_file_extension == '.asm':
         raise ValueError(
             "Wrong argument: ", sys.argv, ", file's extension should be .asm"
         )
 
+    if os.path.getsize(asm_file_path) == 0:
+        raise ValueError("File ", sys.argv, " is empty")
+
+def parse_asm_file(asm_file_path):
+    with open(asm_file_path) as asmfile:
+        asm_file_content = asmfile.read()
+    asm_file_lines = asm_file_content.splitlines()
 
     parsed = []
-
-    with open(asm_file_path) as asmfile:
-        for line in asmfile:
+    for line in asm_file_lines:
+        if line:
             parsed.append(parse_line(line))
+    parsed = "\n".join(parsed)
+    return parsed
 
+def get_asm_and_hack_files_paths():
+    asm_file_path = os.path.abspath(sys.argv[0])
+    asm_file_no_extension, _ = (os.path.splitext(asm_file_path))
     hack_file_path = asm_file_no_extension + '.hack'
+    return asm_file_path, hack_file_path
 
+def assemble():
+    check_arguments()
+    asm_file_path, hack_file_path = get_asm_and_hack_files_paths()
+    parsed_content = parse_asm_file(asm_file_path)
     with open(hack_file_path, 'w') as hackfile:
-        for line in parsed:
-            hackfile.write(line)
-
+        hackfile.write(parsed_content)
     assert os.path.exists(hack_file_path)
 
 
--- nand2tetris/projects/06/test_python_assembler.py
+++ nand2tetris/projects/06/test_python_assembler.py
@@ -28,21 +28,28 @@ class TestArguments(unittest.TestCase):
                 self.assertRaises(ValueError, python_assembler.assemble)
 
 
-class TestLineParsing(unittest.TestCase):
+class TestLineParsingInstructionA(unittest.TestCase):
 
-    def test_A_instruction_1(self):
+    def _test_A_instruction(self, line, expected):
+        parsed = python_assembler.parse_A_instruction(line)
+        self.assertEqual(parsed, expected)
+
+    def test_1(self):
         line = "@1"
-        parsed = python_assembler.parse_A_instruction(line)
         expected = ('0' * 15) + '1'
-        self.assertEqual(parsed, expected)
+        self._test_A_instruction(line, expected)
 
-    def test_A_instuction_biggest_value(self):
+    def test_13107(self):
+        line = "@13107"
+        expected = "0011" + "0011" + "0011" + "0011"
+        self._test_A_instruction(line, expected)
+
+    def test_biggest_value(self):
         line = "@32767"
-        parsed = python_assembler.parse_A_instruction(line)
         expected = '0' + ('1' * 15)
-        self.assertEqual(parsed, expected)
+        self._test_A_instruction(line, expected)
 
-    def test_A_instruction_numbers_too_big(self):
+    def test_numbers_too_big(self):
         lines = ["@32768", "@120000", "@99999999"]
         for line in lines:
             with self.assertRaises(
@@ -50,25 +57,135 @@ class TestLineParsing(unittest.TestCase):
             ):
                 python_assembler.parse_A_instruction(line)
 
+class TestLineParsingInstructionC(unittest.TestCase):
+
+    def _test_instruction_C(self, line, expected_result):
+        result = python_assembler.parse_C_instruction(line)
+        self.assertEqual(expected_result, result)
+
+    def test_simplest_1(self):
+        line = 'D=0'
+        expected = '1110' '1010' '1001' '0000'
+        self._test_instruction_C(line, expected)
+
+    def test_simplest_2(self):
+        line = 'D=1'
+        expected = '1110' '1111' '1101' '0000'
+        self._test_instruction_C(line, expected)
+
+    def test_1(self):
+        line = 'M=D+M'
+        expected = '1111' '0000' '1000' '1000'
+        self._test_instruction_C(line, expected)
+
+    def test_2(self):
+        line = '0;JMP'
+        expected = '1110' '1010' '1000' '0111'
+        self._test_instruction_C(line, expected)
+
+
+class TestInstructionCHelperFunctions(unittest.TestCase):
+
+    def _test_split(self, line, expected_result):
+        result = python_assembler.split_C_instruction(line)
+        self.assertEqual(expected_result, result)
+
+    def test_split_C_no_dest(self):
+        line = 'M;JEQ'
+        expected = ('M', None, 'JEQ')
+        self._test_split(line, expected)
+
+    def test_split_C_no_jump(self):
+        line = 'D=0'
+        expected = ('0', 'D', None)
+        self._test_split(line, expected)
+
+    def test_split_C_full(self):
+        line = 'AM=M|D;JGT'
+        expected = ('M|D', 'AM', 'JGT')
+        self._test_split(line, expected)
+
+    def test_calculate_dest(self):
+        mnemonic_to_binary = (
+            (None,  '000'),
+            ('M',   '001'),
+            ('D',   '010'),
+            ('MD',  '011'),
+            ('A',   '100'),
+            ('AM',  '101'),
+            ('AD',  '110'),
+            ('AMD', '111'),
+        )
+        for mnemonic, expected in mnemonic_to_binary:
+            result = python_assembler.calculate_dest(mnemonic)
+            self.assertEqual(
+                result, expected,
+                f'For {mnemonic} expected {expected}, got {result}'
+            )
+
+    def test_calculate_jump(self):
+        mnemonic_to_binary = (
+            (None,  '000'),
+            ('JGT', '001'),
+            ('JEQ', '010'),
+            ('JGE', '011'),
+            ('JLT', '100'),
+            ('JNE', '101'),
+            ('JLE', '110'),
+            ('JMP', '111'),
+        )
+        for mnemonic, expected in mnemonic_to_binary:
+            result = python_assembler.calculate_jump(mnemonic)
+            self.assertEqual(
+                result, expected,
+                f'For {mnemonic} expected {expected}, got {result}'
+            )
+
+    def test_calculate_comp(self):
+        partial_mnemonic_to_binary = (
+            ('0',   '0101010'),
+            ('A',   '0110000'),
+            ('M',   '1110000'),
+            ('M-D', '1000111'),
+            ('D&A', '0000000'),
+        )
+        for mnemonic, expected in partial_mnemonic_to_binary:
+            result = python_assembler.calculate_comp(mnemonic)
+            self.assertEqual(
+                result, expected,
+                f'For {mnemonic} expected {expected}, got {result}'
+            )
+
 
 class TestFileAssembly(unittest.TestCase):
 
-    def _assemble_file(self, dir_path, content):
-        fname = os.path.join(dir_path, 'test.asm')
+    def _assemble_file(self, dir_path, content, filename='test'):
+        fname = os.path.join(dir_path, f'{filename}.asm')
         with open(fname, mode='w') as tfile:
             tfile.write(content)
-
         with unittest.mock.patch('sys.argv', [fname]):
             python_assembler.assemble()
-        out_file = os.path.join(dir_path, 'test.hack')
+        out_file = os.path.join(dir_path, f'{filename}.hack')
         return out_file
+
+    def _assemble_and_test(self, dir_path, content, expected, filename='test'):
+        assembled_file_path = self._assemble_file(dir_path, content, filename)
+
+        with open(assembled_file_path) as hackfile:
+            result = hackfile.read()
+
+        self.assertEqual(result, expected)
+
 
     def test_parse_empty_file(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
-            expected_out_file = self._assemble_file(tmpdirname, '')
-            with open(expected_out_file) as hackfile:
-                result = hackfile.read()
-            print(result)
+
+            with self.assertRaises(ValueError):
+                expected_out_file = self._assemble_file(tmpdirname, '', 'test')
+
+            self.assertFalse(
+                os.path.exists(os.path.join(tmpdirname, 'test.hack'))
+            )
 
     def test_creates_any_file(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
@@ -77,14 +194,45 @@ class TestFileAssembly(unittest.TestCase):
 
     def test_parse_properly_simplest_file(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
-            expected_out_file = self._assemble_file(tmpdirname, '@1')
-
-            with open(expected_out_file) as hackfile:
-                result = hackfile.read()
             expected = ('0' * 15) + '1'
+            self._assemble_and_test(tmpdirname, '@1', expected)
 
-            self.assertEqual(result, expected)
+    def test_skip_empty_lines(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            content = '@1\n\n\n@1\n\n@1\n'
+            parsed_line = ('0' * 15) + '1'
+            expected = parsed_line + '\n' + parsed_line + '\n' + parsed_line
+            self._assemble_and_test(tmpdirname, content, expected)
 
+    def test_medium_file(self):
+        content = (
+            '@1\n'
+            'D=A\n'
+            '@12\n'
+            'M=D\n'
+            'D=A\n'
+            '\n\n'
+            '@128\n'
+            'D=A\n'
+            'AMD=D-M\n'
+            '\n'
+            '@5\n'
+            '0;JMP\n'
+        )
+        expected = (
+            '0000' '0000' '0000' '0001\n'
+            '111' '0110000' '010' '000\n'
+            '0000' '0000' '0000' '1100\n'
+            '111' '0001100' '001' '000\n'
+            '111' '0110000' '010' '000\n'
+            '0000' '0000' '1000' '0000\n'
+            '111' '0110000' '010' '000\n'
+            '111' '1010011' '111' '000\n'
+            '0000' '0000' '0000' '0101\n'
+            '111' '0101010' '000' '111'
+        )
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            self._assemble_and_test(tmpdirname, content, expected)
 
 
 if __name__ == '__main__':
